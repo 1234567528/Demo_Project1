b1a6b5a46f5f3f3d555e068aea9bf09e0dd5bb63
manasa_m <manasammanasa80@gmail.com>
2025-10-08 15:23:36 +0530

    added new file

---
+++ typecasting-demo.py
@@ -0,0 +1,15 @@
+'''the process of converting one data type to another
+types of typecasting :
+    1.implicit typecasting(automatic)
+    
+      - automatically done by python interpreter.
+      - seen when we perform operations(expressions, assignments) with different datatypes.
+      - lower dataype to bigger datatype.
+      - to avoid data loss.
+      
+    2.explicit typecasting(external)
+    
+      - manually done by user.
+      - we make use of builtin functions.
+      - to avoid unexpected behaviour.'''
+
